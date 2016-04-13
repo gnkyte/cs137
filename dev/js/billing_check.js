85ff33b8a6dc805eb5385cf5318e20b48950d260
jefmark <jlingad@uci.edu>
2016-04-13 10:55:39 -0700

    Added more styles to stylesheet for purchase form and modified layout of purchase form

---
+++ dev/js/billing_check.js
@@ -0,0 +1,11 @@
+function submitForm() {
+    var name = document.getElementById('name').value;
+    var street = document.getElementById('street').value;
+    var city = document.getElementById('city').value;
+    var state = document.getElementById('state').value;
+    var zip = document.getElementById('zip').value;
+    var creditcard = document.getElementById('creditcard').value;
+    var security = document.getElementById('security').value;
+
+//    window.open('mailto:test@example.com?subject=' + q + '&body=' + p);
+}
